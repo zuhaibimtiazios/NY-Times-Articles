cf8a119024bdce821ab5eca8dc0d72b6941854d1
Zuhaib <zuhaib.imtiaz90@gmail.com>
2022-07-01 00:29:41 +0500

    Implemented project structural architecture

---
+++ NY Times Articles/NY Times Articles/MostPopularModule/View/ViewControllers/MostPopularArticlesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NY Times Articles
+//
+//  Created by Zuhaib Imtiaz on 7/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
